6926a52406c4a421e84583c69f6dbe8db68e963c
Brian <brianbtfa@hotmail.com>
2022-04-05 17:57:32 -0500

    Se corrigio el login, funciona ahora

--- lib/Login/login.dart
+++ lib/Login/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../Proyecto Viejo/signin_signup.dart' as request;
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -8,9 +9,9 @@ class Login extends StatefulWidget {
 }
 
 String dropdownValue = 'Dueño';
-String getDropdownValue(){
-    return dropdownValue;
-  } 
+String getDropdownValue() {
+  return dropdownValue;
+}
 
 class _Login extends State<Login> {
   final email = TextEditingController();
@@ -22,6 +23,7 @@ class _Login extends State<Login> {
     _passwordVisible = false;
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -35,13 +37,12 @@ class _Login extends State<Login> {
       body: ListView(
         children: [
           Container(
-            margin: const EdgeInsets.only(top: 70,),
-
-            child: Image.asset("assets/images/Logo.png")
-          ),
+              margin: const EdgeInsets.only(
+                top: 70,
+              ),
+              child: Image.asset("assets/images/Logo.png")),
           Container(
-            margin: const EdgeInsets.only(top:50, left:80),
-
+            margin: const EdgeInsets.only(top: 50, left: 80),
             child: const Text(
               'Inicia sesión con tu cuenta para continuar',
               style: TextStyle(
@@ -55,7 +56,7 @@ class _Login extends State<Login> {
             child: const Text("Correo electrónico"),
           ),
           Container(
-            margin: const EdgeInsets.only(top: 10,left: 25, right: 25),
+            margin: const EdgeInsets.only(top: 10, left: 25, right: 25),
             child: TextField(
               controller: email,
               decoration: InputDecoration(
@@ -66,11 +67,11 @@ class _Login extends State<Login> {
             ),
           ),
           Container(
-            margin: const EdgeInsets.only(top: 20,left: 30),
+            margin: const EdgeInsets.only(top: 20, left: 30),
             child: const Text("Contraseña"),
           ),
           Container(
-            margin: const EdgeInsets.only(top: 10,left: 25, right: 25),
+            margin: const EdgeInsets.only(top: 10, left: 25, right: 25),
             child: TextField(
               controller: password,
               obscureText: !_passwordVisible,
@@ -81,9 +82,7 @@ class _Login extends State<Login> {
                 suffixIcon: IconButton(
                   icon: Icon(
                     Icons.visibility,
-                    color: _passwordVisible
-                        ? Colors.blue
-                        : Colors.grey,
+                    color: _passwordVisible ? Colors.blue : Colors.grey,
                   ),
                   onPressed: () {
                     _passwordVisible = !_passwordVisible;
@@ -94,47 +93,54 @@ class _Login extends State<Login> {
             ),
           ),
           SizedBox(
-            width: 100,
-            child: Padding(
-              padding: EdgeInsets.only(top:10, left: 40, right: 40),
-              child: DropdownButton<String>(
-                value: dropdownValue,
-                icon: const Icon(Icons.arrow_drop_down),
-                elevation: 2,
-                style: const TextStyle(color: Colors.deepPurple),
-                underline: Container(
-                  height: 1,
-                  color: Colors.blue,
+              width: 100,
+              child: Padding(
+                padding: EdgeInsets.only(top: 10, left: 40, right: 40),
+                child: DropdownButton<String>(
+                  value: dropdownValue,
+                  icon: const Icon(Icons.arrow_drop_down),
+                  elevation: 2,
+                  style: const TextStyle(color: Colors.deepPurple),
+                  underline: Container(
+                    height: 1,
+                    color: Colors.blue,
+                  ),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      dropdownValue = newValue!;
+                    });
+                  },
+                  items: <String>['Dueño', 'Empleado']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
                 ),
-                onChanged: (String? newValue) {
-                  setState(() {
-                    dropdownValue = newValue!;
-                  });
-                },
-                items: <String>['Dueño','Empleado']
-                    .map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-              ),
-            )
-          ),
+              )),
           Container(
             margin: const EdgeInsets.only(top: 30, left: 60, right: 60),
-            child:ElevatedButton(
+            child: ElevatedButton(
               onPressed: () async {
-                if (dropdownValue == "Empleado"){
+                var token = await request.signIn(email.text, password.text);
+                if (token != null) {
+                  if (dropdownValue == "Empleado") {
                     Navigator.pushReplacementNamed(context, '/home');
                   } else {
                     Navigator.pushReplacementNamed(context, '/home_mascotas');
                   }
-
+                } else {
+                  const snackBar = SnackBar(
+                    content: Text('Usuario o contraseña incorrecta.'),
+                  );
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                }
               },
-              child: const Text("Iniciar sesión", style: TextStyle(
-                  fontSize: 16
-              ),),
+              child: const Text(
+                "Iniciar sesión",
+                style: TextStyle(fontSize: 16),
+              ),
               style: ElevatedButton.styleFrom(
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(30),
@@ -147,4 +153,3 @@ class _Login extends State<Login> {
     );
   }
 }
-
--- lib/Proyecto Viejo/signin_signup.dart
+++ lib/Proyecto Viejo/signin_signup.dart
@@ -2,13 +2,6 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
 
-void main() async {
-  //Es importante usar el await, de esta forma la variable se espera a que la funcion
-  // termine y ya obtiene el valor
-  var token = await signIn();
-  print(token);
-}
-
 void signUp() async {
   Uri url = Uri.parse("http://localhost:18081/user/owner/signup");
 
@@ -41,28 +34,27 @@ void signUp() async {
   postData();
 }
 
- Future signIn() async {
-  Uri url = Uri.parse("http://localhost:18081/user/login");
+Future signIn(email, password) async {
+  Uri url = Uri.parse("http://10.0.2.2:18081/user/login");
 
-    String email = "mar@mar.com";
-    String password = "Mar";
-    String rol = "Owner";
+  Map data = {
+    "email": email,
+    "password": password,
+  };
 
-    Map data = {
-      "email": email,
-      "password": password,
-    };
-    
-    String jsonObject = json.encode(data);
-    var response = await http.post(url,
-        headers: <String, String>{
-          'Content-Type': 'application/json; charset=UTF-8',
-        },
-        body: jsonObject);
-    // print(response.body);
-  var responseBody = json.decode(response.body);
+  String jsonObject = json.encode(data);
+  var response = await http.post(url,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonObject);
+  // print(response.body);
+  if (response.body.isNotEmpty) {
+    var responseBody = json.decode(response.body);
 
-  var token = responseBody["token"];
-  
-  return token;
+    var token = responseBody["token"];
+
+    return token;
+  }
+  return null;
 }
